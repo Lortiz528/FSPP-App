81610e0d3ca29b00a71dd6c2f13359f0c30d2ab1
Lortiz528 <lortiztech@gmail.com>
2022-08-25 13:28:27 -0400

    collections component

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -30,20 +30,20 @@ CREATE TABLE collections (
 
 -- )
 
--- CREATE TABLE users (
---     id SERIAL NOT NULL PRIMARY KEY,
---     username TEXT NOT NULL,
---     password TEXT NOT NULL,
---     email TEXT NOT NULL,
---     created_at TIMESTAMP DEFAULT NOW()
--- );
+CREATE TABLE users (
+    id SERIAL NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
 
--- CREATE TABLE userAmiibos (
---     id SERIAL NOT NULL PRIMARY KEY,
---     userID INT NOT NULL REFERENCES users(id),
---     name TEXT NOT NULL,
---     series TEXT NOT NULL,
---     is_boxed BOOLEAN,
---     quantity INT DEFAULT 0,
---     image TEXT
--- );
+CREATE TABLE userAmiibos (
+    id SERIAL NOT NULL PRIMARY KEY,
+    userID INT NOT NULL REFERENCES users(id),
+    name TEXT NOT NULL,
+    series TEXT NOT NULL,
+    is_boxed BOOLEAN,
+    quantity INT DEFAULT 0,
+    image TEXT
+);
