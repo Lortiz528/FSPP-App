89cb9be25ec828cd9c69eb1f2f75ddf902209939
Lortiz528 <lortiztech@gmail.com>
2022-08-31 14:30:10 -0400

    syntax in schema

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -36,24 +36,24 @@ CREATE TABLE locations (
     address TEXT NOT NULL,
     website TEXT,
     googleMapLink TEXT
-)
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    email TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
 );
 
-DROP TABLE IF EXISTS userAmiibos;
-CREATE TABLE userAmiibos (
-    id SERIAL NOT NULL PRIMARY KEY,
-    userID INT NOT NULL REFERENCES users(id),
-    name TEXT NOT NULL,
-    series TEXT NOT NULL,
-    is_boxed BOOLEAN,
-    quantity INT DEFAULT 0,
-    image TEXT
-);
+-- DROP TABLE IF EXISTS users;
+-- CREATE TABLE users (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     username TEXT NOT NULL,
+--     password TEXT NOT NULL,
+--     email TEXT NOT NULL,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+
+-- DROP TABLE IF EXISTS userAmiibos;
+-- CREATE TABLE userAmiibos (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     userID INT NOT NULL REFERENCES users(id),
+--     name TEXT NOT NULL,
+--     series TEXT NOT NULL,
+--     is_boxed BOOLEAN,
+--     quantity INT DEFAULT 0,
+--     image TEXT
+-- );
