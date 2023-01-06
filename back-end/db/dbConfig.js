58657c2e746b0134a0139508d458d2b81ed13aa2
Lortiz528 <lortiztech@gmail.com>
2023-01-06 16:39:06 -0500

    undo dbconfig

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -5,7 +5,7 @@ const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
 
 const cn = DATABASE_URL
   ? {
-      connectionString: 'postgres://collection-corner-main-db-069e279c6a93961a6:n6zAvuGkrDsfK95v8ATUra34RV1Ake@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/collection-corner-main-db-069e279c6a93961a6',
+      connectionString: DATABASE_URL,
       max: 30,
       ssl: {
         rejectUnauthorized: false,
